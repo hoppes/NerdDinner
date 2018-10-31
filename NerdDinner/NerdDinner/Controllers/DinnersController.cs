3090c6ae7ff0f7067e71b04e228d42dcb38ca2b5
jonathanmh0716 <jonathanmh0716@10-LTOP-WD-05>
2018-10-30 19:37:37 -0500

    Steps 1 - 4 Nerd Dinners

---
+++ NerdDinner/NerdDinner/Controllers/DinnersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NerdDinner.Controllers
+{
+    public class DinnersController : Controller
+    {
+        // GET: Dinners
+        public void Index()
+        {
+            Response.Write("<h1>Coming Soon</h1>");
+        }
+
+        // GET: /Dinners/Details/[Id]
+        public void Details(int id)
+        {
+            Response.Write("<h1>Details DinnerID: " + id + "<h1>");
+        }
+    }
+}
